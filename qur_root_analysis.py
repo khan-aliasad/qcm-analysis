3fd01f6e0378ed3ac1f11e7376e344741c9aeb43
Ali Khan <khan.aliasad@gmail.com>
2019-11-20 17:29:31 +0400

    updated root analysis src, graphml file for chapter 2, sample graph viz for all chapters entire qcm

--- qur_root_analysis.py
+++ qur_root_analysis.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 This script visualises root data in terms of frequence or occurrence, its relationships with
@@ -16,6 +17,7 @@ __status__ = "dev"
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['axes.facecolor'] = 'black'
 
 from bidi import algorithm as bidialg
 import arabic_reshaper
@@ -50,31 +52,47 @@ def load_corpus_dataframe_from_csv(path = '/Users/alikhan/Downloads/qur/'):
 
 def create_ngrams(quran, col='Root_ar', n=2, separate=True, sep='786'):
 	mask = quran.aya.diff() == 1
-	if separate:
+	if separate: # verse wise pairing, no roots paired across two different adjacent verses, i.e. last root of a verse 
+	# with the first root of the next verse is not paired, if separate = True
 		a = np.insert(quran[col].values, mask.index[mask == True], sep)
+		b = np.insert(quran['position'].values, mask.index[mask == True], sep)
+		# print(len(a))
+		# print(len(b))
 	else:
 		a = quran[col].values
+		b = quran['position'].values
 	a = a[a!=0]
+	b = b[np.nonzero(a)]
+	# print(len(a),len(b))
 	tokens = [x for x in a if type(x) is str]
+	assert(len(a) == len(b) == len(tokens))
 	ngrams = zip(*[tokens[i:] for i in range(n)])
-	return ngrams
+	ngrams_pos = zip(*[b[i:] for i in range(n)])
+	# print(len(list(ngrams)))
+	return ngrams, ngrams_pos
 
 
-def create_graph_edges_dataframe(ngrams, u='one', v='two', sep='786', weight='count'):
-	edges_df = pd.DataFrame(list(ngrams),columns=[u, v])#[" ".join(ngram) for ngram in ngrams])
-	# edges_df = edges_df[edges_df.one != edges_df.two]#.sort_values(by='one')
-	edges_df = edges_df[edges_df != sep].dropna().reset_index()
+def create_graph_edges_dataframe(ngrams, ngrams_pos, u='one', v='two', sep='786', weight='count'):
+	edges_df = pd.DataFrame(list(ngrams),columns=[u, v,])#[" ".join(ngram) for ngram in ngrams])
+	# pos_df = pd.DataFrame(list(ngrams_pos), columns=['pos','pos1'])
+	# edges = pd.concat([edges_df, pos_df], ignore_index=True, axis=1)
+	edges_df = edges_df[edges_df != sep].dropna().reset_index().drop('index',1)
 	edges_df[weight] = 1
-	edges_df = edges_df.groupby([u,v]).sum().reset_index().sort_values(u)
+	temp = edges_df.groupby([u,v]).sum().reset_index()#.sort_values(weight)
+	edges_df = edges_df.drop('count',1).merge(temp, how='left', on=['one','two'])
 	# edges_df[weight] = np.log2(edges_df[weight])*10+1
-	print(edges_df.sort_values(weight))
+	print(temp.sort_values(weight))
+	print('Total # edges: ', len(edges_df))
+	print('Unique edges: ', len(temp))
 	return edges_df
 
 
 def create_graph(edges_df, u='one', v='two', attr_list=['count']):
 	# G=nx.Graph()
+	# G = nx.from_pandas_edgelist(edges_df, \
+	#                       u, v, attr_list, create_using = nx.DiGraph())#MultiDiGraph())
 	G = nx.from_pandas_edgelist(edges_df, \
-	                      u, v, attr_list, create_using = nx.MultiDiGraph())#MultiDiGraph())
+	                      u, v, create_using = nx.MultiDiGraph())#MultiDiGraph())
 	# G.add_nodes_from([x for x in list(set(quran.Root_ar.values)) if type(x) is str])
 	# print("Nodes of graph: ")
 	# print(G.nodes())
@@ -85,29 +103,33 @@ def create_graph(edges_df, u='one', v='two', attr_list=['count']):
 
 def draw_graph(G, node_freq, nodesize_multiplier=40, weight='count', title=''):
 	# shpsize = 30+10*np.log2([root_counts[x] for x in filter(lambda x: shapemap[x]==shp, G.nodes().keys())])
-	shpsize = [node_freq[x]*nodesize_multiplier for x in G.nodes().keys()]
+	shpsize = [node_freq[x]*nodesize_multiplier+10 for x in G.nodes().keys()]
 
-	# pos = nx.spring_layout(G, k=0.8, iterations=10, weight='count')
+	# pos = nx.spring_layout(G, k=0.7, iterations=10, weight='count')
 	# pos = nx.random_layout(G) #, weight='AMOUNT')
 	# pos = nx.circular_layout(G) #, weight='AMOUNT')
 	# pos = nx.shell_layout(G) #, weight='AMOUNT')
 	# pos = nx.spectral_layout(G) #, weight='AMOUNT')
-	maxkey = max(node_freq, key=node_freq.get)
-	print(maxkey, node_freq[maxkey])
+	maxkey = max(node_freq, key=node_freq.get) 
 	# pos = nx.fruchterman_reingold_layout(G, pos= {str(maxkey): (0.1,0.1)}, fixed=[maxkey], k=0.7, iterations=10, weight=weight)
-	pos = nx.fruchterman_reingold_layout(G, k=0.7, iterations=10, weight=weight)
+	pos = nx.fruchterman_reingold_layout(G, k=0.5, iterations=10, weight=weight)
 	
-	nodes = nx.draw_networkx_nodes(G, pos=pos, with_labels=True, node_color='r', alpha=0.3, node_size=shpsize)
+	fig = plt.figure(figsize=(40.10, 20.80))	
+
+	nodes = nx.draw_networkx_nodes(G, pos=pos, with_labels=True, node_color='silver', alpha=0.6, node_size=shpsize)
 	
-	edges = nx.draw_networkx_edges(G, pos=pos, edge_color='g',alpha=0.2,width=[len(G[u][v])/2 for u,v in G.edges()])
+	edges = nx.draw_networkx_edges(G, pos=pos, edge_color='gold',alpha=0.3)#width=[len(G[u][v])/2 for u,v in G.edges()])
 
 	nodelabels = {}
 	for idx, node in enumerate(G.nodes()):
 		nodelabels[node] = bidialg.get_display(arabic_reshaper.reshape(node))
-	labels = nx.draw_networkx_labels(G, pos, nodelabels, alpha=0.5, font_size = 9)
+	labels = nx.draw_networkx_labels(G, pos, nodelabels, font_color='white',alpha=0.7, font_size = 10)
 
 	plt.title(title)
-	# plt.savefig("simple_path.png") # save as png
+	# plt.savefig('simple_path.png') # save as png
+	plt.savefig('./fig/graph.png', dpi=300, facecolor='w', edgecolor='w',
+    orientation='landscape', papertype=None, format='png', transparent=False, bbox_inches=None, pad_inches=0.1,
+    frameon=None)
 	plt.show() # display
 
 
@@ -119,36 +141,50 @@ def draw_subgraph(G, method='breadth', node_of_interest = '', node_freq=None, no
 	else:
 		return 'Please select either \'depth\' or \'breadth\' for the parameter \'method\''
 	I = G.subgraph(I.nodes())
+	# print(G[node_of_interest])
+	# import sys
+	# sys.exit()
 
-	df = pd.DataFrame(index=I.nodes(), columns=I.nodes())
-	for row, datum in nx.shortest_path_length(I):
-	    for col, dist in datum.items():
-	        df.loc[row,col] = dist
+	# df = pd.DataFrame(index=I.nodes(), columns=I.nodes())
+	# for row, datum in nx.shortest_path_length(I):
+	#     for col, dist in datum.items():
+	#         df.loc[row,col] = dist
 
-	df = df.fillna(df.max().max())
+	# df = df.fillna(df.max().max())
 
-	dfspos = nx.kamada_kawai_layout(I, dist=df.to_dict())
+	# dfspos = nx.kamada_kawai_layout(I, dist=df.to_dict())
 
-	list(nx.dfs_labeled_edges(I,node_of_interest))
+	dfspos = nx.fruchterman_reingold_layout(I, k=1, iterations=10)
+
+	# if method is 'breadth':	
+	# 	print(list(nx.bfs_labeled_edges(I,node_of_interest)))
+	# else:
+	# 	print(list(nx.dfs_labeled_edges(I,node_of_interest)))
+
 
 	shpsize = [node_freq[x]*nodesize_multiplier for x in I.nodes().keys()]
 
-	nodes = nx.draw_networkx_nodes(I, pos=dfspos, with_labels=True, node_color='r', alpha=0.3, node_size=shpsize)
-	
-	edges = nx.draw_networkx_edges(I, pos=dfspos, edge_color='g',alpha=0.2, width=[len(I[u][v])/2 for u,v in I.edges()])
+	fig = plt.figure(figsize=(15.10, 20.80))	
+	nodes = nx.draw_networkx_nodes(I, pos=dfspos, with_labels=True, node_color='gold', alpha=0.8, node_size=shpsize)
+	# print([I[u][v]['weight'] for u,v in I.edges()])
+	edges = nx.draw_networkx_edges(I, pos=dfspos, edge_color='silver',alpha=0.3)#, width=[len(I[u][v])/2 for u,v in I.edges()])
 
 	nodelabels = {}
 	for idx, node in enumerate(I.nodes()):
 		nodelabels[node] = bidialg.get_display(arabic_reshaper.reshape(node))
-	labels = nx.draw_networkx_labels(I, dfspos, nodelabels, alpha=0.5, font_size = 9)
+	labels = nx.draw_networkx_labels(I, dfspos, nodelabels, font_color='white', alpha=0.9, font_size = 10)
 
 	plt.title('Root: ' + bidialg.get_display(arabic_reshaper.reshape(node_of_interest))+ 
-				', Freq: ' + str(node_freq[node_of_interest]) +
+				' [Freq: ' + str(node_freq[node_of_interest]) +
 				', Roots: ' + str(len(I.nodes())) +
-				', Cooccurrences: '+ str(len(I.edges())) + '\nIn [' +
-				title + ']')
+				', Cooccurrences: '+ str(len(I.edges())) + ']\nIn ' +
+				title)
+	plt.savefig('./fig/'+qur_func.arabic_to_buc(node_of_interest) + '.png', dpi=300, facecolor='w', edgecolor='w',
+    orientation='landscape', papertype=None, format='png', transparent=False, bbox_inches=None, pad_inches=0.1,
+    frameon=None)
 	plt.show()
-	
+	# from networkx.drawing.nx_agraph import write_dot
+	# write_dot(I, 'test.dot')
 
 ###########################
 
@@ -156,22 +192,24 @@ def draw_subgraph(G, method='breadth', node_of_interest = '', node_freq=None, no
 if __name__ == '__main__':
 
 	path = '/Users/alikhan/Downloads/qur/qcm-analysis/'
-	sur = None
-	sz=5
+	sur =2
+	sz=15
 	if sur is not None:
 		sz=40
 	analysand = 'Root_ar'#'Lemma_ar'
-	node_of_interest = u'ذكر'#'كون'#u'حقق'#u'ذكر'#'*kr'#'kwn'#'qwl' #u'ذكر' u'ارض'. 'حرم' 'فعل' 'حرم' 'ﻏﻀﺐ'
+	node_of_interest = u'كون'#'فقه'#'دين'#'فقه'#'شرع'#u'حقق'#u'ذكر'#'*kr'#'kwn'#'qwl' #u'ذكر' u'ارض'. 'حرم' 'فعل' 'حرم' 'ﻏﻀﺐ'
 	method = 'breadth'
 	
 	quran, qtoc = load_corpus_dataframe_from_csv(path = path)
 	if sur is not None:
 		quran = quran[quran.sura == sur].reset_index()
 
-	# second = first.groupby(['aya','word']).agg('FORM_ar').apply(''.join).reset_index()
-	bigrams = create_ngrams(quran, col=analysand, n=2, separate=True, sep='786')	
+	quran['position'] = quran['sura'].astype(str) + ':' + quran['aya'].astype(str)
 
-	edges_df = create_graph_edges_dataframe(bigrams, u='one', v='two', sep='786', weight='count')
+	# second = first.groupby(['aya','word']).agg('FORM_ar').apply(''.join).reset_index()
+	bigrams, bigrams_pos = create_ngrams(quran, col=analysand, n=2, separate=True, sep='786')	
+
+	edges_df = create_graph_edges_dataframe(bigrams, bigrams_pos, u='one', v='two', sep='786', weight='count')
 
 	G = create_graph(edges_df, u='one', v='two', attr_list=['count'])
 
@@ -201,11 +239,12 @@ if __name__ == '__main__':
 	if sur is not None:
 		title = str(*qtoc[qtoc['No.'] == sur].values.tolist())[1:-1] + '\nRoots: ' + str(len(G.nodes())) +', Cooccurrences: '+ str(len(G.edges()))
 	else:
-		title = 'The Holy Quran, Roots: ' + str(len(G.nodes())) +', Cooccurrences: '+ str(len(G.edges()))
+		title = 'The Holy Quran [Roots: ' + str(len(G.nodes())) +', Cooccurrences: '+ str(len(G.edges())) + ']'
 	
-	# draw_graph(G, node_freq=root_counts, nodesize_multiplier = sz, weight='count', title=title)
+	nx.write_graphml(G, path + 'test.graphml')
+	draw_graph(G, node_freq=root_counts, nodesize_multiplier = sz, weight='count', title=title)
 
-	draw_subgraph(G, method=method, node_of_interest = node_of_interest, nodesize_multiplier = sz, node_freq=root_counts, title= title)
+	# draw_subgraph(G, method=method, node_of_interest=node_of_interest, nodesize_multiplier = sz, node_freq=root_counts, title= title)
 	#############################################
 	# check how many times a given degree occurs:
 	degrees = [a + ' ' + str(G.degree[a]) for a in G.nodes]
